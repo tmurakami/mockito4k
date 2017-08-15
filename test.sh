b6272acffeb01be52319ff6d81b1cf6610c83b8e
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-08-15 19:27:21 +0900

    Update Kotlin to 1.1.4

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for test_kotlin_version in '1.0.7' '1.1.3-2'; do
+for test_kotlin_version in '1.0.7' '1.1.4'; do
     for test_mockito_version in '2.7.0' '2.8.47' 'latest.release'; do
         cmd="./gradlew -PtestKotlinVersion=${test_kotlin_version} -PtestMockitoVersion=${test_mockito_version} test"
         echo ${cmd}
