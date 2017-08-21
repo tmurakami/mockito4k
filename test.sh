d7d0b453b7e1ba7086fe07f0a705a44227859968
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-08-21 19:11:45 +0900

    More tests

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for test_kotlin_version in '1.0.7' '1.1.4-2'; do
-    for test_mockito_version in '2.7.2' '2.8.47' 'latest.release'; do
-        for test_mockito_type in 'core' 'inline'; do
-            cmd="./gradlew -PtestKotlinVersion=${test_kotlin_version} -PtestMockitoVersion=${test_mockito_version} -PtestMockitoType=${test_mockito_type} clean test"
+    for test_mockito_version in '2.7.0' '2.7.22' '2.8.55' 'latest.release'; do
+        for options in '' '-PusingInlineMockMaker'; do
+            cmd=`echo "./gradlew clean test -PtestKotlinVersion=${test_kotlin_version} -PtestMockitoVersion=${test_mockito_version} ${options}" | sed -e 's/[[:space:]]*$//'`
             echo "\n> ${cmd}"
             eval ${cmd} || exit $?
         done
