a0041529a4677d88bda228803ad565738ba47b46
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-08-27 08:01:31 +0900

    Test with Mockito 2.9.0

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for test_kotlin_version in '1.0.7' '1.1.4-2'; do
-    for test_mockito_version in '2.7.0' '2.7.22' '2.8.47'; do
+    for test_mockito_version in '2.7.0' '2.7.22' '2.8.47' '2.9.0'; do
         for using_inline_mock_maker in false true; do
             echo '========================================='
             echo " Kotlin: ${test_kotlin_version}"
