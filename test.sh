dd3a491d77b5f93c8c2f71b05e28a8c0f96ed9cf
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-08-30 19:58:48 +0900

    Update Kotlin to 1.1.4-3

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for test_kotlin_version in '1.0.7' '1.1.4-2'; do
+for test_kotlin_version in '1.0.7' '1.1.4-3'; do
     for test_mockito_version in '2.7.0' '2.7.22' '2.8.47' '2.9.0'; do
         for using_inline_mock_maker in false true; do
             echo '========================================='
