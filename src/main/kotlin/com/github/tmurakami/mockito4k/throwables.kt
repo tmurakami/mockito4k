029a90b9c23d70ee3ef99dbe940bcc5a6497a5b0
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-09-13 20:52:38 +0900

    Refactor

--- src/main/kotlin/com/github/tmurakami/mockito4k/throwables.kt
+++ src/main/kotlin/com/github/tmurakami/mockito4k/throwables.kt
@@ -2,8 +2,8 @@ package com.github.tmurakami.mockito4k
 
 import org.mockito.internal.configuration.GlobalConfiguration
 
-private val myPackage = Init::class.java.run { name.removeSuffix(simpleName) }
 private val configuration = GlobalConfiguration()
+private val packagePrefix = Init::class.java.run { name.removeSuffix(simpleName) }
 
 internal fun <T> filterStackTrace(function: () -> T): T =
     try {
@@ -11,11 +11,10 @@ internal fun <T> filterStackTrace(function: () -> T): T =
     } catch (e: Exception) {
         if (configuration.cleansStackTrace()) {
             // We do not use the `StackTraceCleaner` extension because multiple StackTraceCleaners are not allowed. If
-            // we used the extension, libraries with their own StackTraceCleaner (e.g. dexmaker-mockito) might not work
-            // with this library.
+            // we used the extension, libraries with their own StackTraceCleaner (e.g. dexmaker-mockito) might not work.
             @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
             (e as java.lang.Throwable).run {
-                stackTrace = stackTrace.filterNot { it.className.startsWith(myPackage) }.toTypedArray()
+                stackTrace = stackTrace.filterNot { it.className.startsWith(packagePrefix) }.toTypedArray()
             }
         }
         throw e
