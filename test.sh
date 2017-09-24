cda16c3ff6397c0cf3508c4f54dc1aa27a613ee1
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-09-25 01:27:49 +0900

    Update Kotlin to 1.1.50

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for test_kotlin_version in '1.0.7' '1.1.4-3'; do
+for test_kotlin_version in '1.0.7' '1.1.50'; do
     for test_mockito_version in '2.7.0' '2.10.0' 'latest.release'; do
         for using_inline_mock_maker in false true; do
             echo '========================================='
