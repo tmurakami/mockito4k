dca23da3be3fa707343873e9d933b2b1e9c4b970
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2018-03-25 06:25:51 +0900

    Update dependencies

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for test_kotlin_version in '1.1.61' '1.2.21'; do
-    for test_mockito_version in '2.7.0' '2.15.0' 'latest.release'; do
+for test_kotlin_version in '1.1.61' '1.2.31'; do
+    for test_mockito_version in '2.7.0' '2.17.0' 'latest.release'; do
         for using_inline_mock_maker in false true; do
             echo '========================================='
             echo " Kotlin: ${test_kotlin_version}"
